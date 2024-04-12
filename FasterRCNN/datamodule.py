8b8ac490e156ae2f2c3aa56d2aeea6ac915386c4
Bård Wikmark <bardwikmark2001@gmail.com>
2024-04-12 14:13:49 +0200

    feat: Dataloader functions as intended

--- FasterRCNN/datamodule.py
+++ FasterRCNN/datamodule.py
@@ -4,10 +4,14 @@ from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms, utils, tv_tensors
 from torchvision.transforms import v2
 import torch
+from torchvision.tv_tensors import BoundingBoxes
+
+
+from cityscapes import Cityscapes
 
 
 class CityscapesDataModule(pl.LightningDataModule):
-    def __init__(self, batch_size=64, num_workers=11, data_root="/work/baardrw/cityscapesDataset", train_split_ratio=0.8, valid_labels = None):
+    def __init__(self, batch_size=30, num_workers=11, data_root="/home/bard/Documents/cityscapes", train_split_ratio=0.8, valid_labels = None):
         super().__init__()
         self.batch_size = batch_size
         self.num_workers = num_workers
@@ -23,13 +27,19 @@ class CityscapesDataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         # Split the dataset into train and validation sets
-        self.train_dataset = datasets.wrap_dataset_for_transforms_v2(datasets.Cityscapes(
-            root=self.data_root, mode='fine', target_type='instance', split='train', target_transform=self.target_transform, transform=self.get_transforms("train")))
+        self.train_dataset = Cityscapes(
+            root=self.data_root, mode='fine', target_type='polygon', split='train', transforms= self.get_transforms("train"))
         
-        self.train_dataset = Subset(self.train_dataset, np.arange(0, 100)).dataset
+        # change line 191 in cityscapes.py to target = Image.open(self.targets[index][i]).convert("L")
+        # self.train_dataset[0][1]
+        # self.train_dataset[0][0]
+
         
-        self.val_dataset = datasets.wrap_dataset_for_transforms_v2(datasets.Cityscapes(
-            root=self.data_root, mode='fine', target_type='instance', split='val'))
+        # print(self.train_dataset[0][1]['labels'])
+        # print(self.train_dataset[0][1]['masks'])
+
+        self.val_dataset = Cityscapes(
+            root=self.data_root, mode='fine', target_type='instance', split='val')
         
         
     def collate_fn(self, batch):
@@ -38,7 +48,7 @@ class CityscapesDataModule(pl.LightningDataModule):
         return images, targets
         
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True, shuffle=True, collate_fn=self.collate_fn)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True, shuffle=True,  collate_fn=lambda batch: tuple(zip(*batch)))
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True, shuffle=False)
@@ -49,13 +59,12 @@ class CityscapesDataModule(pl.LightningDataModule):
         return DataLoader(test_dataset, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True, shuffle=False)
 
     def target_transform(self, target):
-        return target
-        target['masks'] = [tv_tensors.Mask(mask) for mask in target['masks']]
+        # print(target)
         return target
     
     def get_transforms(self, split, label=False):
-        mean = [0.4914, 0.4822, 0.4465]
-        std = [0.2023, 0.1994, 0.2010]
+        mean = [0.4914]
+        std = [0.2023]
 
         shared_transforms = [
             v2.ToImage(),
@@ -64,21 +73,13 @@ class CityscapesDataModule(pl.LightningDataModule):
 
         if split == "train":
             
-            if label:
-                return v2.Compose(
-                    [
-                        v2.RandomPhotometricDistort(p=1.0),
-                        v2.RandomRotation(degrees=10)
-                    ]
-                    )
-            
-            return v2.Compose(
-            [
-                # v2.ToImage(),
-                v2.RandomPhotometricDistort(p=1.0),
-                v2.RandomRotation(degrees=10)
-            ]
-            )                   
+            return v2.Compose([
+                    # v2.ToTensor(),
+                    v2.RandomResizedCrop(size=(512, 512), antialias=True),
+                    v2.Grayscale(num_output_channels=1),
+                    v2.ToDtype(torch.float32, scale=True),
+                    # v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                    ])                  
             
             return v2.Compose([
                 *shared_transforms,
@@ -100,34 +101,31 @@ class CityscapesDataModule(pl.LightningDataModule):
 
 
 if __name__ == '__main__':
+    import torchvision
+    
+    
     # Test the data loader
     loader = CityscapesDataModule()
     loader.setup()
+    print(len(loader.train_dataset))
     
     train_loader:DataLoader = loader.train_dataloader()
-    
     # Get one batch from dataloader
     batch = next(iter(train_loader))
-    print(batch[0][0].shape)
-    print(batch[1][0]['masks'].shape)
+    # print(batch[1][0])
+    # print(batch[1][1])    
     
+    for i in range(len(batch[0])):
+        # # transform train_ds[0][0] to uint8
+        all_bbs = batch[1][i]['boxes']
         
-    
-    
-    # # transform train_ds[0][0] to uint8
-    # all_masks = train_ds[0][1]['masks'].squeeze(
-    #     1)  # Remove channel dimension
-    
-
-    # image_tensor = v2.functional.to_dtype(train_ds[0][0], torch.float32)
-    # image_uint8 = (image_tensor * 255).type(torch.uint8)
-    
-    # # img = utils.draw_segmentation_masks(image_uint8, all_masks)
-    # img = utils.draw_segmentation_masks(image_uint8, all_masks)
-
-    # import matplotlib.pyplot as plt
-    # print(img.numpy().transpose(1, 2, 0).shape)
-    # showable_im = img.numpy().transpose(1, 2, 0)
-
-    # plt.imshow(showable_im)
-    # plt.show()()
+        image_tensor = v2.functional.to_dtype(batch[0][i], torch.float32)
+        image_uint8 = (image_tensor * 255).type(torch.uint8)
+        
+        img = utils.draw_bounding_boxes(image_uint8, all_bbs, width=5)
+        #img = utils.draw_segmentation_masks(batch[0][0], batch[1][0] )
+        import matplotlib.pyplot as plt
+        # print(img.numpy().transpose(1, 2, 0).shape)
+        # showable_im = img.numpy().transpose(1, 2, 0)
+        # plt.imshow(img.numpy().transpose(1, 2, 0))
+        plt.imsave(f"ims/test{i}.png", img.numpy().transpose(1, 2, 0))
--- Notes.md
+++ Notes.md
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-## Adapting pretrained model to Grayscale images
-
-2 approaches:
-1) Adding additional channels to each greyscale image
-2) Modifying the first convolutional layer of the pretrained network
-
-https://towardsdatascience.com/transfer-learning-on-greyscale-images-how-to-fine-tune-pretrained-models-on-black-and-white-9a5150755c7a
-
-=======
 ## Training of Mask R-CNN on City scapes
 
 Implementation: We apply our Mask R-CNN models with
@@ -20,4 +10,3 @@ a single scale of 1024 pixels. We use a mini-batch size of
 for 24k iterations, starting from a learning rate of 0.01 and
 reducing it to 0.001 at 18k iterations. It takes ∼4 hours of
 training on a single 8-GPU machine under this setting.
->>>>>>> 4998a77631ef37a54e31c31b7b625eaf3a698ddf
